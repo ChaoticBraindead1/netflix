11e11a3fdee34de3880e70ae02090e7166868ed1
ChaoticBraindead1 <mname1946@gmail.com>
2022-08-07 22:52:33 +0200

    fake commit

--- src/components/UpperForm/index.tsx
+++ src/components/UpperForm/index.tsx
@@ -8,7 +8,7 @@ const UpperForm = () => {
   return (
     <>
       <span className={styles.steps}>STEP <b>1</b> OF <b>2</b></span>
-      <h1 className={styles.title}>Set up your credit or debit cardd</h1>
+      <h1 className={styles.title}>Set up your credit or debit card</h1>
       <div className={styles.cards}>
         <img src={visa} alt="" />
         <img src={mastercard} alt="" />
